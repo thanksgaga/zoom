23b167870ae2b581dc6688c68212ef9519d3a0e7
thanks.gaga <coach.gayoung@gmail.com>
2021-12-27 23:05:00 +0800

    Simple Chat  with users Nickname

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,10 +8,42 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
-
-console.log("hello");
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+	console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+	sockets.push(socket); //list of the sockets connected to this server
+	socket["nickname"] = "Anonymous";
+
+	console.log("Connected to Browser ✅");
+	socket.on("close", onSocketClose);
+
+	socket.on("message", (msg) => {
+		const messageUTF8 = msg.toString("utf-8");
+		const messageString = JSON.parse(messageUTF8);
+		switch (messageString.type) {
+			case "new_message":
+				sockets.forEach((aSocket) =>
+					aSocket.send(`${socket.nickname}: ${messageString.payload}`),
+				);
+				console.log("server side message", messageString.payload);
+				break;
+			case "nickname":
+				socket["nickname"] = messageString.payload;
+				break;
+		}
+	});
+});
+
+server.listen(3000, handleListen);
