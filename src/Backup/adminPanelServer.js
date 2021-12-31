b6ed175b400bbc9bda72efbd32434380a0eac083
thanks.gaga <coach.gayoung@gmail.com>
2021-12-31 16:49:26 +0800

    clean up ----

--- src/Backup/adminPanelServer.js
+++ src/Backup/adminPanelServer.js
@@ -1,17 +1,31 @@
-import http from "http";
-import SocketIO from "socket.io";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer, {
+	cors: {
+		origin: ["https://admin.socket.io"],
+		credentials: true,
+	},
+});
+
+instrument(wsServer, {
+	auth: false,
+});
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
-
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+//const wsServer = SocketIO(httpServer);
+
 httpServer.listen(3000, handleListen);
